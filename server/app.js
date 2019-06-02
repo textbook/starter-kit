7dc826028d48e4627df12f61ec6e4fd0063ef0e5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-06-02 12:54:31 +0100

    Add logging to server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import path from "path";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/api", (_, res) => {
   res.json({ message: "Hello, world!" });
