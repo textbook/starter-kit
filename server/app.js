8ca2b110035f404b1e196ef78b20446972ebd596
Jonathan Sharpe <jsharpe@pivotal.io>
2019-06-04 11:45:35 +0100

    Redirect to HTTPS in production

--- server/app.js
+++ server/app.js
@@ -3,10 +3,17 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 
+import { httpsOnly } from "./helpers";
+
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
+
+if (app.get("env") === "production") {
+  app.enable("trust proxy");
+  app.use(httpsOnly());
+}
 
 app.get("/api", (_, res) => {
   res.json({ message: "Hello, world!" });
