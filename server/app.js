23f44caef8e71e0a1e73ff83280220e675cd510e
Jonathan Sharpe <jsharpe@pivotal.io>
2019-06-04 10:23:20 +0100

    Add Helmet for common security measures

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan("dev"));
 
 app.get("/api", (_, res) => {
