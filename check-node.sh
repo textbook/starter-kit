89a82829b46d4ec0ac028717ff4146a7bab80612
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-01-15 19:38:06 +0000

    Make sure build (and deploy) stage runs in expected Node version

---
+++ check-node.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+ACTUAL="$(node -v)"
+EXPECTED="v$(cat .nvmrc)"
+
+echo "Node: $ACTUAL"
+echo "NPM: v$(npm -v)"
+
+if [[ "$ACTUAL" == "$EXPECTED" ]];
+then echo 'Node version OK';
+else echo 'Node version mismatch' && exit 1;
+fi
