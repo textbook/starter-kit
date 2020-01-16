4e5405588a67821a88dff01bd1d53cd72167b05d
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-01-16 22:34:53 +0000

    Add more information to build info

--- bin/build-info.sh
+++ bin/build-info.sh
@@ -5,5 +5,20 @@ set -euo pipefail
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DIR="$HERE/../dist/static"
 
-if [ -z "$(git status --porcelain)" ] && [ -d "$DIR" ]; then echo "Built: $(date)
-From: $(git show -s --format='%h %s')" > "$DIR/build-info.txt"; fi
+mkdir -p "$DIR"
+
+LINES=("Built: $(date)")
+
+TRAVIS=${TRAVIS:-}
+if [ ! -z "$TRAVIS" ]
+then LINES+=("By: Travis build $TRAVIS_BUILD_NUMBER" "URL: $TRAVIS_BUILD_WEB_URL")
+else LINES+=("By: $(whoami)")
+fi
+
+LINES+=("From: $(git show -s --format='%h %s')")
+
+if [ ! -z "$(git status --porcelain)" ]
+then LINES+=("With changes:" "$(git status --porcelain)")
+fi
+
+printf "%s\n" "${LINES[@]}" > "$DIR/build-info.txt"
