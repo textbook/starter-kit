1d2100f480fc5cfb70a824631f14f77a1d673a89
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-01-16 23:23:54 +0000

    Try to make build info work in Heroku

--- bin/build-info.sh
+++ bin/build-info.sh
@@ -10,15 +10,20 @@ mkdir -p "$DIR"
 LINES=("Built: $(date)")
 
 TRAVIS=${TRAVIS:-}
-if [ ! -z "$TRAVIS" ]
-then LINES+=("By: Travis build $TRAVIS_BUILD_NUMBER" "URL: $TRAVIS_BUILD_WEB_URL")
-else LINES+=("By: $(whoami)")
+if [ ! -z "$TRAVIS" ]; then
+  LINES+=("By: Travis build $TRAVIS_BUILD_NUMBER" "URL: $TRAVIS_BUILD_WEB_URL")
+else
+  LINES+=("By: $(whoami)")
 fi
 
-LINES+=("From: $(git show -s --format='%h %s')")
+if [ -d "$HERE/../.git" ]; then
+  LINES+=("From: $(git show -s --format='%h %s')")
 
-if [ ! -z "$(git status --porcelain)" ]
-then LINES+=("With changes:" "$(git status --porcelain)")
+  if  [ ! -z "$(git status --porcelain || '')" ]; then
+    LINES+=("With changes:" "$(git status --porcelain)")
+  fi
+else
+  LINES+=("From: $SOURCE_VERSION")
 fi
 
 printf "%s\n" "${LINES[@]}" > "$DIR/build-info.txt"
