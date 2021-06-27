61c36da1ed606cdfeb0ac69d0e787c4609c5ffd6
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-06-27 22:55:18 +0100

    Update connection handling for MongoDB
    Seemed to cause problems when trying to access the DB for testing.

--- bin/files/db/mongo-db.js
+++ bin/files/db/mongo-db.js
@@ -1,4 +1,4 @@
-import { createConnection } from "mongoose";
+import mongoose from "mongoose";
 
 const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
 
@@ -8,16 +8,18 @@ const configuration = {
 	useUnifiedTopology: true,
 };
 
-let connection;
-
 export const connectDb = async () => {
 	try {
-		connection = await createConnection(dbUrl, configuration);
-		console.log("MongoDB connected to", connection.name);
+		await mongoose.connect(dbUrl, configuration);
+		console.log("MongoDB connected to", mongoose.connection.name);
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
 	}
 };
 
-export const disconnectDb = () => connection.close();
+export const disconnectDb = async () => {
+	if (mongoose.connection) {
+		await mongoose.connection.close();
+	}
+};
