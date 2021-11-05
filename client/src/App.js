db073e91605ff33867362e2715d9c1dcd1b8b833
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-11-05 21:44:43 +0000

    Migrate router usage for v6
    Guide: https://reactrouter.com/docs/en/v6/upgrading/v5

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 const App = () => (
-	<Switch>
-		<Route path="/" exact><Home /></Route>
-		<Route path="/about/this/site"><About /></Route>
-	</Switch>
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/about/this/site" element={<About />} />
+	</Routes>
 );
 
 export default App;
