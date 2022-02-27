81bf8bb892eb04a256fa80bc7e42f922a85655e3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-02-27 10:05:43 +0000

    Consistent log format

--- bin/files/db/server.js
+++ bin/files/db/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 server.on("listening", () => {
 	const addr = server.address();
 	const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-	logger.info("Listening on %s", bind);
+	logger.info("listening on: %s", bind);
 });
 
 process.on("SIGTERM", () => server.close(() => disconnectDb()));
