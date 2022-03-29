a81d0720c32cb4c8d176616bb6a40f092310263d
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-03-29 19:34:51 +0100

    Migrate to React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-ReactDOM.render((
+createRoot(document.getElementById("root")).render((
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>
-), document.getElementById("root"));
+));
