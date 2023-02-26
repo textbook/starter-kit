65cb6a2510e2d8d925fffdea4c00a8d780b5f0e8
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-02-26 11:47:34 +0000

    Use NPM prefix rather than pushd [skip ci]

--- bin/up-to-date.sh
+++ bin/up-to-date.sh
@@ -7,19 +7,20 @@ if [ $# -gt 0 ]; then
 fi
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT="$HERE/.."
 
-pushd "$ROOT"
-  npm ci
+useNpm() {
+  npm --prefix="$HERE/.." "$@"
+}
 
-  if [ -z "$(npm outdated)" ]; then
-    echo 'All up to date'
-    exit 0
-  fi
+useNpm ci
 
-  npm update --save
-  npm run ship
-  git add package{,-lock}.json
-  git commit --message 'Apply in-range dependency updates'
-  git push
-popd
+if [ -z "$(useNpm outdated)" ]; then
+  echo 'All up to date'
+  exit 0
+fi
+
+useNpm update --save
+useNpm run ship
+git add package{,-lock}.json
+git commit --message 'Apply in-range dependency updates'
+git push
