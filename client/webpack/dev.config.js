d8cf36327572f8bbd4625549b5ccfb74dc4fff4d
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-07-24 16:13:16 +0100

    Support running dev mode in GitHub Codespaces

--- client/webpack/dev.config.js
+++ client/webpack/dev.config.js
@@ -2,13 +2,22 @@ const { merge } = require("webpack-merge");
 
 const common = require("./common.config");
 
+/**
+ * Support running in {@link https://github.com/features/codespaces GitHub Codespaces}.
+ */
+const codespace = process.env.CODESPACE_NAME;
+
+const port = 3000;
+
 module.exports = merge(common, {
 	devtool: "inline-source-map",
 	devServer: {
+		allowedHosts: codespace && [`${codespace}-${port}.preview.app.github.dev`],
+		client: { webSocketURL: { port: codespace && 443 } },
 		historyApiFallback: {
 			disableDotRule: true,
 		},
-		port: 3000,
+		port,
 		proxy: [
 			{
 				context: ["/api"],
