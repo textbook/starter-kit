12c705d50d2e9f81a3250b384891143d4a2f187b
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-01-09 22:28:25 +0000

    Add basic server logs

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import express from "express";
+import morgan from "morgan";
 
 const application = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+application.use(morgan("dev"));
 
 application.get("/healthz", (_, res) => res.sendStatus(200));
 
