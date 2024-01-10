39afd344318b0c27609b402aabad74b42d97020e
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-01-10 23:07:21 +0000

    Get logs from container

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 import config from "./utils/config.js";
+import logger from "./utils/logger.js";
 
 const { port } = config;
 
-app.listen(port, () => console.log(`listening on ${port}`));
+app.listen(port, () => logger.info(`listening on ${port}`));
