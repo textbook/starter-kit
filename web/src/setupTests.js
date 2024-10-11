13a3a5d4d54a15cc21d765c407a715730ff189e3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-10-11 10:48:39 +0100

    Strip out unneeded workaround

--- web/src/setupTests.js
+++ web/src/setupTests.js
@@ -1,10 +1,7 @@
 import "@testing-library/jest-dom";
 import { setupServer } from "msw/node";
-import { setGlobalOrigin } from "undici";
 
 export const server = setupServer();
-
-setGlobalOrigin(window.location.href); // see mswjs/msw#1625
 
 beforeAll(() => server.listen());
 
