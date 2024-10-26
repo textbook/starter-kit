6f57e03cbbc49d6951d0ccb15c23dc92be27d480
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-10-26 15:17:01 +0100

    Disable fewer ESLint rules
    - Comment why when disabling
    - Use config instead of accessing process.env again

--- api/migrations/config.cjs
+++ api/migrations/config.cjs
@@ -1,21 +1,16 @@
 const { join } = require("node:path");
 
-const config = require("../utils/config.cjs");
+const { dbConfig, logLevel, production } = require("../utils/config.cjs");
 
 const migrationConfig = {
 	"ignore-pattern": "(config|template)\\.cjs$",
 	"migrations-dir": __dirname,
 	"template-file-name": join(__dirname, "template.cjs"),
-	url: config.dbConfig,
+	url: dbConfig,
 };
 
-if (
-	/* eslint-disable no-restricted-syntax */
-	process.env.NODE_ENV?.toLowerCase() !== "production" &&
-	process.env.LOG_LEVEL?.toLowerCase() === "debug"
-	/* eslint-enable no-restricted-syntax */
-) {
-	/* eslint-disable-next-line no-console */
+if (logLevel === "debug" && !production) {
+	/* eslint-disable-next-line no-console -- app logger not available here */
 	console.debug("migrating with %O", migrationConfig);
 }
 
