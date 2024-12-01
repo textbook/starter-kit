8d4a01f25de2b688f33039037b6ec79158d1c662
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-12-01 12:17:00 +0000

    Upgrade React Router to v7

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 
 import "./App.css";
 import About from "./pages/About.jsx";
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 
 import App from "./App.jsx";
 import "./index.css";
