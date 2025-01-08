5f6e631e576766eabd9f90b0ef098d74fd612ae3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-08 14:42:05 +0000

    Demonstrate automated accessibility testing

--- e2e/tests/about.spec.js
+++ e2e/tests/about.spec.js
@@ -1,7 +1,15 @@
+import AxeBuilder from "@axe-core/playwright";
 import { test, expect } from "@playwright/test";
 
 test("can be visited", async ({ page }) => {
 	await page.goto("/nested/about/path");
 
 	await expect(page.getByRole("heading", { level: 1 })).toHaveText("About");
+});
+
+test("meets a11y requirements", async ({ page }) => {
+	await page.goto("/nested/about/path");
+	const axeBuilder = new AxeBuilder({ page }).withTags("wcag2a");
+	const { violations } = await axeBuilder.analyze();
+	expect(violations).toHaveLength(0);
 });
--- e2e/tests/home.spec.js
+++ e2e/tests/home.spec.js
@@ -1,3 +1,4 @@
+import AxeBuilder from "@axe-core/playwright";
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
@@ -19,4 +20,11 @@ test("shows server status", async ({ page }) => {
 	await page.goto("/");
 
 	await expect(page.getByText("Server says: Hello, world!")).toBeAttached();
+});
+
+test("meets a11y requirements", async ({ page }) => {
+	await page.goto("/");
+	const axeBuilder = new AxeBuilder({ page }).withTags("wcag2a");
+	const { violations } = await axeBuilder.analyze();
+	expect(violations).toHaveLength(0);
 });
