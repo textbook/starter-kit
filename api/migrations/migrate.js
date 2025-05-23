ea35520052ac0fe64e3af0b1cf1cc5bbdcf93fde
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-05-23 11:23:00 +0100

    Support count and redo

--- api/migrations/migrate.js
+++ api/migrations/migrate.js
@@ -5,13 +5,20 @@ import { runner } from "node-pg-migrate";
 import config from "../utils/config.js";
 
 const {
-	positionals: [direction],
+	positionals: [direction, rawCount],
 } = parseArgs({ allowPositionals: true });
 
-if (!direction || !["up", "down"].includes(direction)) {
-	throw new Error("usage: npm run migration <up|down>");
+if (!direction || !["down", "redo", "up"].includes(direction)) {
+	throw new Error("usage: npm run migration <up|down|redo> [count]");
 }
 
 const { migrationConfig } = config.init();
 
-await runner({ ...migrationConfig, direction });
+const count = rawCount ? parseInt(rawCount, 10) : undefined;
+
+if (direction === "redo") {
+	await runner({ ...migrationConfig, count, direction: "down" });
+	await runner({ ...migrationConfig, count: count ?? 1, direction: "up" });
+} else {
+	await runner({ ...migrationConfig, count, direction });
+}
